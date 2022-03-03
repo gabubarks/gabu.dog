59a20d8aed9a462d40431dd413f184f2bf8c8dff
Gabu <wildgabu@gmail.com>
2022-03-03 16:55:37 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <RouterView />
+</template>
+
+<style>
+body {
+    background-image: url("/static/img/bg.jpg");
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,41 @@
+<template>
+<main class="text-slate-900 font-light tracking-wide">
+  <div class="flex grow flex-col items-center justify-center p-5">
+    <div class="container max-w-screen-lg p-10 rounded-2xl bg-slate-100/75 text-center">
+      <img class="mx-auto rounded-xl max-w-[300px] mb-4 border-slate-100" src="/static/img/pfp_500px.png">
+      <h1 class="text-6xl text-gablue mb-2">hi!! i'm gabu!!</h1>
+      <p class="text-2xl mb-16">
+        30 | he/him
+      </p>
+      <img class="mx-auto mb-2 rounded-lg border-2 border-slate-100" src="/static/img/gabu-ref-sheet-2022.jpg">
+      <p class="text-xl mb-16">
+        art by <a class="text-gablue" href="https://twitter.com/DDen009">@DDen009</a>
+        (<a class="text-gablue" href="/static/img/gabu-ref-sheet-2022.jpg">full size</a>)
+      </p>
+      
+      <p class="text-2xl">
+        you can find me on these websites
+      </p>
+      <p class="text-xl">
+        <a class="text-gablue" href="https://twitter.com/gabubarks">Twitter</a>
+        |
+        <a class="text-gablue" href="https://www.furaffinity.net/user/wildgabu/">FurAffinity</a>
+        |
+        <a class="text-gablue" href="https://toyhou.se/gabubarks">Toyhouse</a>
+        |
+        <a class="text-gablue" href="https://github.com/gabubarks">GitHub</a>
+      </p>
+    </div>
+  </div>
+</main>
+</template>
+
+<script>
+import Links from '../components/Links.vue'
+
+export default {
+    components: {
+        Links,
+    },
+}
+</script>
